1a400485fe89d089429928025e96459f44085164
natali3z <Natalie.Zeakes@gmail.com>
2019-07-10 10:36:39 -0400

    Add files via upload

---
+++ java.js
@@ -0,0 +1,85 @@
+alert("Hello! This is a page about Natalie Zeakes!!");
+// <img onmuseover.src="width:25px;height:25px;"></img>
+// <img onmouseover="bigImg6(this)" onmouseout="normalImg6(this)" border="0" src="https://lh3.googleusercontent.com/_tSGULl7mAUbamNvDqtOgbOAfdbbUO-VsZPLpY3Gne1neAgtRXZfdK3hBnE8jmt7xzDN0gDuE2bKN6SXGGrb-v-kzZDBWGKp8JWDJpDUOacSp_JmaeiS4ljWoKDizmEGDL6sa_TCa53qL_HgJzdVN6ex31XNadYoAjPDZ2dWSYc81xrlHaVHPhWEjQKTjagzdrAYDo8_eSmhg3Eras0WvIObvrInzDtClurRT89vCLb0BIA_xVEKukiz9TflqcgwlpejB6xSUpJTAhiXtSNc3jm5uZb2xHTa3YvwDjTrp7FTnwGw_RlnYu04CPm5re06IVgEF7qUv2hVACGftFl4ZkjWpD80CwaOQt1weWYnGWOLJOo6pJEKklAAsXL0HwvvYaOW1oW5GFUJ0ZHo7Q27w6JTLvwp5Krza0bQdO6P9KhsjlfnkKhOhdtdtUTLePYpBG2vy3f_52ibKbfqJfedewPTiY_LjlbmL6BN1-DVv1w_cdpDETce3fYnRs91kbPca5wykUYaQVP3isf-BU-mHOh92BBgix5bj6Koq2RyOHwl9oxCOQMm4f595MODA5TaiMxzVYJVkvPoeOLxsabPtIN5FonKbvOkOf4Vu7r5yOZugmXdLbUgZpKwRM5nSX7H3sV339Ahz-gs30dYwvhOtizQUIOtE1Q=w687-h915-no" alt="Smiley" width="305px" height="390px">
+
+// <p>The function bigImg() is triggered when the user mouse over the image. This function enlarges the image.</p>
+// <p>The function normalImg() is triggered when the mouse pointer is moved out of the image. That function sets the height and width of the image back to normal.</p>
+
+function bigImg1(x) {
+  x.style.height = "50%";
+  x.style.width = "50%";
+}
+
+function normalImg1(x) {
+  x.style.height = "410px";
+  x.style.width = "395px";
+}
+
+function bigImg2(x) {
+  x.style.height = "50%";
+  x.style.width = "50%";
+}
+
+function normalImg2(x) {
+  x.style.height = "410px";
+  x.style.width = "510px";
+}
+
+function bigImg3(x) {
+  x.style.height = "50%";
+  x.style.width = "50%";
+}
+
+function normalImg3(x) {
+  x.style.height = "398px";
+  x.style.width = "408px";
+}
+
+function bigImg4(x) {
+  x.style.height = "50%";
+  x.style.width = "50%";
+}
+
+function normalImg4(x) {
+  x.style.height = "345px";
+  x.style.width = "370px";
+}
+
+function bigImg5(x) {
+  x.style.height = "120%";
+  x.style.width = "120%";
+}
+
+function normalImg5(x) {
+  x.style.height = "345px";
+  x.style.width = "865px";
+}
+
+function bigImg6(x) {
+  x.style.height = "50%";
+  x.style.width = "50%";
+}
+
+function normalImg6(x) {
+  x.style.height = "390px";
+  x.style.width = "305px";
+}
+
+function bigImg7(x) {
+  x.style.height = "50%";
+  x.style.width = "50%";
+}
+
+function normalImg7(x) {
+  x.style.height = "410px";
+  x.style.width = "320px";
+}
+
+function centerImg(y) {
+  y.float.center;
+}
+
+
+function bio(z) {
+  z.
+}
